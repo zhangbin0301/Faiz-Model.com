48b301d37e6084febb5788fda8d92a72d0b7996b
zhangbin0301 <zhangbin0301@qq.com>
2025-12-01 19:36:36 +0800

    Update superapp.py

--- superapp.py
+++ superapp.py
@@ -997,7 +997,7 @@ if MODAL_AVAILABLE:
     )
 
     # 设置Modal应用名称 - 修改这里可以更改部署到Modal平台的项目名称
-    app = modal.App("faizapp-web", image=image)
+    app = modal.App("superapp-web", image=image)
 
     # 设置保活频率、容器个数、CPU、内存、地区域
     @app.function(
