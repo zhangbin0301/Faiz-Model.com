022761bc13c39f5b507eeda35b929d4358e7bf36
zhangbin0301 <zhangbin0301@qq.com>
2025-12-22 20:19:27 +0800

    Update Faiznewapp.py

--- Faiznewapp.py
+++ Faiznewapp.py
@@ -455,22 +455,19 @@ async def generate_links(argo_domain):
             capture_output=True, text=True, timeout=10
         )
         data = json.loads(result.stdout)
-        isp = data.get('isp', 'Unknown').replace(' ', '_')
-        country = data.get('country_code', '')
+        isp = requests.get("https://ipconfig.ggff.net", timeout=5).content.decode("utf-8").strip()
+        country = data.get('isp', '')
         ISP = f"{isp}-{country}" if country else isp
     except Exception as e:
         print(f"Failed to get ISP info: {e}")
-        ISP = "Unknown"
+        ISP = "🇺🇳 联合国"
 
     time.sleep(2)
-    VMESS = {"v": "2", "ps": f"{NAME}-{ISP}", "add": CFIP, "port": CFPORT, "id": UUID, "aid": "0", "scy": "none", "net": "ws", "type": "none", "host": argo_domain, "path": "/vmess-argo?ed=2560", "tls": "tls", "sni": argo_domain, "alpn": "", "fp": "chrome"}
+    VMESS = {"v": "2", "ps": f"{ISP}-{NAME}", "add": CFIP, "port": CFPORT, "id": UUID, "aid": "0", "scy": "none", "net": "ws", "type": "none", "host": argo_domain, "path": "/vmess-argo?ed=2560", "tls": "tls", "sni": argo_domain, "alpn": "", "fp": "chrome"}
 
     list_txt = f"""
-vless://{UUID}@{CFIP}:{CFPORT}?encryption=none&security=tls&sni={argo_domain}&fp=chrome&type=ws&host={argo_domain}&path=%2Fvless-argo%3Fed%3D2560#{NAME}-{ISP}
-
 vmess://{ base64.b64encode(json.dumps(VMESS).encode('utf-8')).decode('utf-8')}
 
-trojan://{UUID}@{CFIP}:{CFPORT}?security=tls&sni={argo_domain}&fp=chrome&type=ws&host={argo_domain}&path=%2Ftrojan-argo%3Fed%3D2560#{NAME}-{ISP}
     """
     
     with open(os.path.join(FILE_PATH, 'list.txt'), 'w', encoding='utf-8') as list_file:
