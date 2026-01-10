a67f9b2f31e8e6539682a414e3ab705b6934b5ce
zhangbin0301 <zhangbin0301@qq.com>
2026-01-10 08:21:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -512,7 +512,7 @@ async def generate_links(argo_domain):
     VMESS = {"v": "2", "ps": f"{ISP}-{NAME}", "add": CFIP, "port": CFPORT, "id": UUID, "aid": "0", "scy": "none", "net": "ws", "type": "none", "host": argo_domain, "path": "/vmess-argo?ed=2560", "tls": "tls", "sni": argo_domain, "alpn": "", "fp": "chrome"}
 
     list_txt = f"""
-vless://{UUID}@{CFIP}:{CFPORT}?encryption=none&security=tls&sni={argo_domain}&fp=chrome&type=ws&host={argo_domain}&path=%2Fvless-argo%3Fed%3D2560#{ISP}-{NAME}
+vless://{UUID}@{CFIP}:{CFPORT}?encryption=none&security=tls&sni={argo_domain}&fp=chrome&type=ws&host={argo_domain}&path=%2Fvless-argo%3Fed%3D2560#{ISP} | {NAME}
 
 
     """
